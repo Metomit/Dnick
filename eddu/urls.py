d1fea8694f82b7ddbd06f6533e2112409f72d031
Metomit <85799617+Metomit@users.noreply.github.com>
2022-10-01 00:25:06 +0200

    Final commit

---
+++ eddu/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+#URLconf
+urlpatterns = [
+    path('login', views.login_req),
+    path('', views.login_req),
+    path('select_course', views.actual_login),
+    path('logout', views.logout),
+    path('video', views.video),
+    path('stream', views.stream),
+    path('about', views.about),
+    path('books', views.books),
+]
